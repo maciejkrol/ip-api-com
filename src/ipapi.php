6cca6d3f606a3ae6bf59c115f0eefd22c543cffe
maciejkrol <mail@maciejkrol.pl>
2016-04-11 17:34:56 +0200

    added docu

--- src/ipapi.php
+++ src/ipapi.php
@@ -2,14 +2,29 @@
 
 namespace maciejkrol\ipapicom;
 
+/**
+ * Represents all what is known about the ad viewer and its viewing context.
+ * Data is represented as nestes array.
+ */
 class ipapi {
     
     private $key = null;
 
+    /**
+     * Creates new instance of the ipapi class initialized with the
+     * provided API key.
+     * @param string $_key 
+     */ 
     public function __construct ($_key) {
         $this->key = $_key;
     }
     
+    /**
+     * Returs localization data for the specified IP or null
+     * if there was a problem.
+     * @param string $_ip
+     * @return array|null
+     */    
     public function locate ($_ipAddress, $_raw = false) {
                 
         $url = $this->buildURL($_ipAddress); 
@@ -39,6 +54,11 @@ class ipapi {
         return $result;
     }
     
+    /**
+     * Builds and API request url.
+     * @param string $_ipAddress
+     * @return string
+     */
     private function buildURL ($_ipAddress) {
         
         $url = 'https://pro.ip-api.com/'
